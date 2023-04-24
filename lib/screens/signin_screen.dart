b19dbae00ce743ff931fe1bc1d0bf2ee7e8ae1d7
Kathurjan <91419114+Kathurjan@users.noreply.github.com>
2023-04-24 14:00:43 +0200

    made frontend/landingpage need to add input fields along with connect to firebase for now

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,44 @@
+import 'package:elosystem/utils/color_utils.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  hexStringToColor("fdbb2d"),
+                  hexStringToColor("22c1c3"),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.1,
+            left: MediaQuery.of(context).size.width * 0.5 - 80,
+            child: Image.asset(
+              'assets/images/easv-logo-transparent.png',
+              width: 160,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
