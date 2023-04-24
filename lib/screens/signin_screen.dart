94a15714fab362a894ef23bbf8e654246dd2a221
Kathurjan <91419114+Kathurjan@users.noreply.github.com>
2023-04-24 19:06:44 +0200

    further changes made to the login, along with routing to sign up page

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,3 +1,5 @@
+import 'package:elosystem/reusable_widgets/resuable_widgets.dart';
+import 'package:elosystem/screens/signup_screen.dart';
 import 'package:elosystem/utils/color_utils.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,9 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,18 +32,83 @@ class _SignInScreenState extends State<SignInScreen> {
             ),
           ),
           Positioned(
-            top: MediaQuery.of(context).size.height * 0.1,
-            left: MediaQuery.of(context).size.width * 0.5 - 80,
+            top: MediaQuery
+                .of(context)
+                .size
+                .height * 0.1,
+            left: MediaQuery
+                .of(context)
+                .size
+                .width * 0.5 - 80,
             child: Image.asset(
               'assets/images/easv-logo-transparent.png',
               width: 160,
               fit: BoxFit.contain,
             ),
           ),
+          Positioned(
+            top: MediaQuery
+                .of(context)
+                .size
+                .height * 0.55,
+            left: 32,
+            right: 32,
+            child: resuableTextField(
+                "Email", Icons.person_3_outlined, false, _emailController),
+          ),
+          Positioned(
+            top: MediaQuery
+                .of(context)
+                .size
+                .height * 0.65,
+            left: 32,
+            right: 32,
+            child: resuableTextField(
+                "PassWord", Icons.lock_clock_outlined, true,
+                _passwordTextController),
+          ),
+          Positioned(
+            top: MediaQuery
+                .of(context)
+                .size
+                .height * 0.75,
+            left: 32,
+            right: 32,
+            child: signInButton(context, true, () {}),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.85,
+            left: 0,
+            right: 0,
+            child: signUpOption(),
+          ),
+
         ],
       ),
     );
   }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          "Don't have an account?",
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
 }
-
-
