b57b11951dce208ab46e7ac197ef12c5f4addf63
DoncusSpelunkus <marcuspetersen@live.dk>
2023-04-26 14:19:54 +0200

    QR and Button
    QR and Straight out buttons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,12 @@
+import 'package:elosystem/screens/quiz_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:elosystem/reusable_widgets/resuable_widgets.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import '../utils/auth_service.dart';
+
+import '../utils/color_utils.dart';
+import '../utils/slideAnimation.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,18 +16,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-            padding: const EdgeInsets.only(left: 50.0, right: 50.0),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: MediaQuery.of(context).size.height * 0.1,
-                  left: 0,
-                  right: 0,
-                  child: Column(children: [
+        body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            hexStringToColor("fdbb2d"),
+            hexStringToColor("22c1c3"),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Padding(
+          padding: const EdgeInsets.only(left: 50.0, right: 50.0),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.1,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
                     Image.asset(
                       "assets/images/placeholder-profile.png",
                       width: 100,
@@ -28,52 +49,59 @@ class _HomeScreenState extends State<HomeScreen> {
                       fit: BoxFit.contain,
                     ),
                     Text(
-                      "Marcus Petersen asdadsdasadsadsadadadsadsasd",
+                      "Marcus Petersen",
                       style: TextStyle(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Image.asset(
-                      "assets/images/QR-placeholder.png",
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.contain,
+                    QrImage(
+                      data: "Marcus",
+                      size: 100,
+                      // You can include embeddedImageStyle Property if you
+                      //wanna embed an image from your Asset folder
+                      embeddedImageStyle: QrEmbeddedImageStyle(
+                        color: Colors.white,
+                        size: const Size(
+                          100,
+                          100,
+                        ),
+                      ),
                     ),
-                  ]),
+                  ]
+                      .map((widget) => Padding(
+                            padding: const EdgeInsets.only(bottom: 16, top: 16),
+                            child: widget,
+                          ))
+                      .toList(),
                 ),
-                Positioned(
-                    top: MediaQuery.of(context).size.height * 0.5,
-                    left: 0,
-                    right: 0,
-                  child: Column(children: [
-                    ElevatedButton(
-                      style: TextButton.styleFrom(
+              ),
+              Positioned(
+                  top: MediaQuery.of(context).size.height * 0.55,
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    children: [
+                      RoutingButton("Quiz", context, () async {
+                          Navigator.push(context, SlideAnimationRoute(child: QuizScreen(), slideRight: true)); // Navigate to the screen after successful sign in
+                        }
                       ),
-                      onPressed: () { },
-                      child: Text('Assignment'),
-                    ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(
+                      SizedBox(
+                        height: 20,
                       ),
-                      onPressed: () { },
-                      child: Text('Quiz'),
-                    ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(
+                      // RoutingButton("Score", "path"),
+                      SizedBox(
+                        height: 20,
                       ),
-                      onPressed: () { },
-                      child: Text('Score'),
-                    ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(
+                      // RoutingButton("Stats", "path"),
+                      SizedBox(
+                        height: 20,
                       ),
-                      onPressed: () { },
-                      child: Text('Stats'),
-                    ),
-                  ],)
-                ),
-              ],
-            )));
+                      // RoutingButton("Quiz", "path"),
+                    ],
+                  )),
+            ],
+          )),
+    ));
   }
 }
