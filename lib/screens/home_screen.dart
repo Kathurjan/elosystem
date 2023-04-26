4608097f40af2abd91870f47df5c9f9e7c3ca96e
DoncusSpelunkus <marcuspetersen@live.dk>
2023-04-26 10:54:45 +0200

    Hello assets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,33 +12,37 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Positioned(
-          top: MediaQuery.of(context).size.height * 0.1,
-          child: Column(children: [
-            Image.asset(
-              "assets/images/placeholder-profile.png",
-              width: 100,
-              height: 100,
-              fit: BoxFit.contain,
-            ),
-            Text(
-              "Name",
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Image.asset(
-              "assets/images/QR-placeholder.png",
-              width: 100,
-              height: 100,
-              fit: BoxFit.contain,
-            ),
-          ]),
-        ),
-      ],
-    ));
+        body: Padding(
+            padding: const EdgeInsets.only(left: 50.0, right: 50.0),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.1,
+                  left: 0,
+                  right: 0,
+                  child: Column(children: [
+                    Image.asset(
+                      "assets/images/placeholder-profile.png",
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(
+                      "Marcus Petersen asdadsdasadsadsadadadsadsasd",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/images/QR-placeholder.png",
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.contain,
+                    ),
+                  ]),
+                ),
+              ],
+            )));
   }
 }
